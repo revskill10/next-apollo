f9d1063240b2cdc21a58c867843add50933e1240
Dung Truong <checkraiser11@gmail.com>
2018-12-18 07:23:34 +0700

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,3 @@
+import makeApp from 'kms-web-app'
+
+export default makeApp('edu-1.herokuapp.com/v1alpha1/graphql')
---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import {useStore} from 'easy-peasy'
+import gql from 'graphql-tag'
+import Query from 'kms-web-components/lib/lib/query'
+
+const Websocket = () => {
+  const websocketStatus = useStore(state => state.websocket.status)
+  return <div>{websocketStatus}</div>
+}
+
+const testQuery = gql`
+  query{
+    sche_ages{
+      id
+      age_name
+      from_month
+      to_month
+    }
+  }
+`
+
+
+const testSubscription = gql`
+  subscription {
+    sche_ages{
+      id
+      age_name
+      from_month
+      to_month
+    }
+  }
+`
+
+const Page = () => {
+  return (
+    <div>
+      <Websocket />
+      <Query
+        query={testQuery}
+        subscription={testSubscription}>
+        {
+          data => <div>{JSON.stringify(data)}</div>
+        }
+        </Query>
+    </div>
+  )
+}
+
+Page.getInitialProps = async ({apolloClient, fetchPolicy}) => {  
+  await apolloClient.query({query: testQuery, ...fetchPolicy})    
+}
+
+export default Page
